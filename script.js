4a0590868653493f5558ee3d6b74917d20ca9c8d
Amar <ambadypradeepcoc@gmail.com>
2023-02-10 12:42:30 +0530

    bluring effect added to the cards in student dashboard

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-//index
+//card1
 function zoomIn1(){
     var x1 = document.getElementById('card1');
     var x2 = document.getElementById('card2');
     var x3 = document.getElementById('card3');
-    x1.style
-   
-}
-function normal1(){
-    var x1 = document.getElementById('card1');
-    var x2 = document.getElementById('card2');
-    var x3 = document.getElementById('card3');
+    x1.style.filter = "blur(0px)";
+    x2.style.filter = "blur(9px)";
+    x3.style.filter = "blur(9px)"; 
 }
 /*card2*/
 function zoomIn2(){
     var x1 = document.getElementById('card1');
     var x2 = document.getElementById('card2');
     var x3 = document.getElementById('card3');
+    x1.style.filter = "blur(9px)";
+    x2.style.filter = "blur(0px)";
+    x3.style.filter = "blur(9px)";
    
-}
-function normal2(){
-    var x1 = document.getElementById('card1');
-    var x2 = document.getElementById('card2');
-    var x3 = document.getElementById('card3');
 }
 /*card3*/
 function zoomIn3(){
     var x1 = document.getElementById('card1');
     var x2 = document.getElementById('card2');
     var x3 = document.getElementById('card3'); 
+    x1.style.filter = "blur(9px)";
+    x2.style.filter = "blur(9px)";
+    x3.style.filter = "blur(0px)";
 }
-function normal3(){
+function normal1(){
     var x1 = document.getElementById('card1');
     var x2 = document.getElementById('card2');
     var x3 = document.getElementById('card3');
+    x1.style.filter = "blur(0px)";
+    x2.style.filter = "blur(0px)";
+    x3.style.filter = "blur(0px)";
 }
