2e6a3b8f0f623846c478670680c9292bcd8a2b24
Amar <ambadypradeepcoc@gmail.com>
2023-02-10 12:19:15 +0530

    login page frontend completed with added dropdown menu

---
+++ script.js
@@ -0,0 +1,36 @@
+//index
+function zoomIn1(){
+    var x1 = document.getElementById('card1');
+    var x2 = document.getElementById('card2');
+    var x3 = document.getElementById('card3');
+    x1.style
+   
+}
+function normal1(){
+    var x1 = document.getElementById('card1');
+    var x2 = document.getElementById('card2');
+    var x3 = document.getElementById('card3');
+}
+/*card2*/
+function zoomIn2(){
+    var x1 = document.getElementById('card1');
+    var x2 = document.getElementById('card2');
+    var x3 = document.getElementById('card3');
+   
+}
+function normal2(){
+    var x1 = document.getElementById('card1');
+    var x2 = document.getElementById('card2');
+    var x3 = document.getElementById('card3');
+}
+/*card3*/
+function zoomIn3(){
+    var x1 = document.getElementById('card1');
+    var x2 = document.getElementById('card2');
+    var x3 = document.getElementById('card3'); 
+}
+function normal3(){
+    var x1 = document.getElementById('card1');
+    var x2 = document.getElementById('card2');
+    var x3 = document.getElementById('card3');
+}
